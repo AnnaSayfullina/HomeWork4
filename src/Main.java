2199eedc514497e9433906816028cde6981ad47e
anna <anna@sayf.su>
2022-12-06 11:07:25 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,11 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
+        int temperature = 4;
+        if (temperature >= 5) {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        }
     }
 }
